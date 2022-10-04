96cf24de2cb8370c47f5e57928a51e36ab33d978
RAHUL404R <ragulr404@gmail.com>
2022-10-04 23:52:57 +0530

    Load src code added

--- src/get_data.py
+++ src/get_data.py
@@ -18,6 +18,8 @@ def get_data(config_path):
     df = pd.read_csv(data_path, sep=",", encoding='utf8')
     return df
 
+# extra commit
+
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
